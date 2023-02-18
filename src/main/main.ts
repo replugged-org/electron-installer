68fa97d3287732691a2c65672890e81252ece6ce
Albert Portnoy <albert@albertp.dev>
2023-02-17 19:05:12 -0600

    Do not open devtools on launch

--- src/main/main.ts
+++ src/main/main.ts
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
 const isDebug = process.env.NODE_ENV === "development" || process.env.DEBUG_PROD === "true";
 
 if (isDebug) {
-  require("electron-debug")();
+  require("electron-debug")({ showDevTools: false });
 }
 
 const installExtensions = (): void => {
